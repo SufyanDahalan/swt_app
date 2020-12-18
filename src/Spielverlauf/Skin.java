250813de3038c77282c3dbc3ada137d81c47f292
1651992 <tobias.geissler@uni-wuppertal.de>
2020-12-18 23:54:01 +0100

    fix read file issue; refresh picture

--- src/Spielverlauf/Skin.java
+++ src/Spielverlauf/Skin.java
@@ -13,19 +13,17 @@ import java.nio.file.Paths;
 
 public class Skin {
 
-	private String skin_graphic; // Grafikdatei
+	private File skin_graphic_file; // Grafikdatei
 	private JSONObject skin_catalog; // Grafikaktalog
-
-	BufferedImage image = getImage( "dig_red_usd" );
 
 	// Load Skin
 	public Skin(File skinDir, String skinname){
-		File skin_graphic_file = new File(skinDir, skinname+".png");
+
+		skin_graphic_file = new File(skinDir, skinname+".png");
 		File skin_catalog_file = new File(skinDir, skinname+".json");
-		// TODO: fix read FIle not path
 
 		try {
-			skin_catalog = new JSONObject(skin_catalog_file.toString());
+			skin_catalog = new JSONObject(new String(Files.readAllBytes(Paths.get(skin_catalog_file.getPath()))));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -37,10 +35,11 @@ public class Skin {
 
 		BufferedImage dest = null;
 		try {
-			dest = ImageIO.read(new File(skin_graphic)).getSubimage(pic_val.getInt(0), pic_val.getInt(1), pic_val.getInt(2), pic_val.getInt(3));
+			dest = ImageIO.read(skin_graphic_file).getSubimage(pic_val.getInt(0), pic_val.getInt(1), pic_val.getInt(2), pic_val.getInt(3));
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
+
 		return dest;
 	}
 
