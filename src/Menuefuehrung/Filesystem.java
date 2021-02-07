b0249e87753ff98eae5056907d8ae1ebc54b7c92
1651992 <tobias.geissler@uni-wuppertal.de>
2021-02-07 12:17:11 +0100

    added "witch" Skin (to test it: adjust the skinname in Filesystem.java)

--- src/Menuefuehrung/Filesystem.java
+++ src/Menuefuehrung/Filesystem.java
@@ -7,7 +7,9 @@ public interface Filesystem {
 
     String rootDir = "bin/";
 
+    //String skinName = "witch_skin"; // Skinnname
     String skinName = "original_skin"; // Skinnname
+
     String skinfolder_name = rootDir + "skins/"; // ./skin/sink_original.json,...
 
     String levelfolder_name = rootDir+"level/"; // ./level/level-01.json ...
